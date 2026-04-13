4925ad1c8561df9fcdbf8b00cae6d16a3543fe20
megabytis <166828632+megabytis@users.noreply.github.com>
2026-04-13 11:54:58 +0530

    feat: added fileUploadRouter with main app

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,13 @@ import express from "express";
 
 const app = express();
 
+import fileUploadRouter from "./routes/file-upload.js";
+
 app.get("/me", (req, res, next) => {
   res.send("Hey there Miku!");
 });
+
+app.use("/api/v1/", fileUploadRouter);
 
 app.listen(5000, () => {
   console.log("App is listening on port 5000......");
