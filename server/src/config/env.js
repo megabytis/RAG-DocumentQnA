240c7cefd59664075cb2bc8381b544424b8dd5b4
megabytis <166828632+megabytis@users.noreply.github.com>
2026-04-13 12:45:43 +0530

    fix: environment variable name modified

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -4,7 +4,7 @@ import path from "path";
 dotenv.config({ path: path.resolve(process.cwd(), "../.env") });
 
 const env = {
-  BACKEND_URL: process.env.BACKEND_URL || "http://localhost:8000",
+  AI_SERVICE_URL: process.env.AI_SERVICE_URL || "http://localhost:8000",
 };
 
 export default env;
