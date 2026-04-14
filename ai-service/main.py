f22c7d7367e86a1be327e22916e3257b8227b176
megabytis <166828632+megabytis@users.noreply.github.com>
2026-04-14 20:58:23 +0530

    feat: implement recursive text chunking and add FastAPI ingestion endpoint

---
+++ ai-service/main.py
@@ -0,0 +1,49 @@
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import os
+import logging
+from loaders import load_file
+from chunkers import chunk_text
+
+app = FastAPI()
+
+
+class IngestRequest(BaseModel):
+    doc_id: str
+    file_path: str
+
+
+# Configure logging to file
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler(),  # This keeps console output too
+    ],
+)
+
+
+@app.post("/ingest")
+async def ingest(request: IngestRequest):
+    logging.info(f"Received: doc_id={request.doc_id}, file_path={request.file_path}")
+
+    # checking wheather the file exists or not
+    if not os.path.isfile(request.file_path):
+        raise HTTPException(status_code=400, detail="file not found")
+
+    # extracting file type
+    file_type = os.path.splitext(request.file_path)[1]
+
+    # reading contents from the file using the filepath and calling our loaders/ module
+    result = load_file(file_path=request.file_path, file_type=file_type)
+    # print(result)
+    
+    # chunking
+    chunks = chunk_text(result)
+    # for i, chunk in enumerate(chunks[:2]):
+    #     print(f"Chunk {i}: {chunk[:100]}...")
+
+    # response
+    return {"status": "indexed", "doc_id": request.doc_id, "chunks": 0}
