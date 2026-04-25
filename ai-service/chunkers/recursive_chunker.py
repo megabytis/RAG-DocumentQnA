f9b993d301ec51df2553caaf808b73fcf5f17543
megabytis <166828632+megabytis@users.noreply.github.com>
2026-04-25 20:47:36 +0530

    feat: added document_id along with each chunk

--- ai-service/chunkers/recursive_chunker.py
+++ ai-service/chunkers/recursive_chunker.py
@@ -1,7 +1,9 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def chunk_text(text, chunk_size=500, overlap=50):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
-    chunks=splitter.split_text(text)
-        
+def chunk_text(text, doc_id=None, chunk_size=500, overlap=50):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=overlap)
+    chunks = splitter.split_text(text)
+    
+    if doc_id:
+        return [{"id": f"{doc_id}_chunk_{i}", "text": chunk} for i, chunk in enumerate(chunks)]
     return chunks
